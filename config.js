9ae9e3bbb75b731f6b93f6661edb28cc5ba71069
thgilciffart <73938431+thgilciffart@users.noreply.github.com>
2024-04-11 11:28:40 +1000

    lol

---
+++ config.js
@@ -0,0 +1,20 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://gleeful-sunshine-918d10.netlify.app",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjFjZjY1NDNjMjk4OWMzZjc4OTc5YWQ5YzZjOWVjMCIsInN1YiI6IjY2MTcyOGY1N2UxMmYwMDE3Y2YxMjM1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cczhby35FHCBU0ut9CZU5yRa8T_aL-muR-M-MuGNKKQ",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: "https://backend-production-65e9.up.railway.app",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+};
